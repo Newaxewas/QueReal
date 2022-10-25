de05a1c124207f58026473fa3d96ff14eac87217
Андрей Стороженко <andreystorozhen4o@gmail.com>
2022-10-25 10:35:21 +0300

    Setup Program

--- src/QueReal.PL/Program.cs
+++ src/QueReal.PL/Program.cs
@@ -1,6 +1,50 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using QueReal.BLL;
+using QueReal.BLL.Interfaces;
+using QueReal.DAL;
+using QueReal.PL;
 
-app.MapGet("/", () => "Hello World!");
+namespace Queral.Pl;
 
-app.Run();
+internal static class Program 
+{
+    public static void Main(string[] args)
+    {
+        WebApplication
+            .CreateBuilder(args)
+            .ConfigureBuilder()
+            .Build()
+            .Configure()
+            .Run();
+    }
+
+    private static WebApplicationBuilder ConfigureBuilder(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddDal(builder.Configuration)
+            .AddBll(builder.Configuration)
+            .AddPl(builder.Configuration);
+
+        return builder;
+    }
+
+    private static WebApplication Configure(this WebApplication app)
+    {
+        app.InitDatabase();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        return app;
+    }
+
+    private static void InitDatabase(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var databaseService = scope.ServiceProvider.GetService<IDatabaseService>();
+
+        databaseService.InitDatabaseAsync().Wait();
+    }
+}
