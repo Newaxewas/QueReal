b8d84aa31a35bfcf21a0b85cc997e620aac39c06
Андрей Стороженко <andreystorozhen4o@gmail.com>
2022-10-25 10:32:15 +0300

    Add repository interface and its implementation

---
+++ src/QueReal.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace QueReal.DAL.Interfaces
+{
+    internal interface IRepository<T>
+    {
+        public Task<Guid> CreateAsync(T entity);
+
+        public Task UpdateAsync(T entity);
+
+        public Task DeleteAsync(T entity);
+
+        public Task<IEnumerable<T>> GetAllAsync(
+            Expression<Func<T, bool>> predicate = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderFunc = null,
+            int skipCount = 0,
+            int takeCount = 0);
+
+        public Task<int> CountAsync(
+            Expression<Func<T, bool>> predicate = null,
+            int skipCount = 0,
+            int takeCount = 0);
+    }
+}
