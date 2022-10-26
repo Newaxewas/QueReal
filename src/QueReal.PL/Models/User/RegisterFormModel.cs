3332b125701132eb70d5a2824441c27cc48d0a30
Андрей Стороженко <andreystorozhen4o@gmail.com>
2022-10-26 11:49:49 +0300

    Add view pages

--- src/QueReal.PL/Models/User/RegisterFormModel.cs
+++ src/QueReal.PL/Models/User/RegisterFormModel.cs
@@ -9,5 +9,9 @@ namespace QueReal.PL.Models.User
 
         [Required, MinLength(ModelConstants.User_Password_MinLength), MaxLength(ModelConstants.User_Password_MaxLength)]
         public string Password { get; set; }
+
+        [Display(Name = "Confirm password")]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
     }
 }
