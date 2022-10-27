27e5dcb8e345cfb65a59edac5138559f8778a3b5
Андрей Стороженко <andreystorozhen4o@gmail.com>
2022-10-27 16:44:33 +0300

    Add QuestService

---
+++ src/QueReal.BLL/Interfaces/IQuestService.cs
@@ -0,0 +1,11 @@
+﻿namespace QueReal.BLL.Interfaces
+{
+    public interface IQuestService
+    {
+        public Task<Guid> CreateAsync(Quest questModel);
+
+        public Task<Quest> GetAsync(Guid id);
+
+        public Task<IEnumerable<Quest>> GetAllAsync(int pageNumber, int takeCount);
+    }
+}
---
+++ src/QueReal.BLL/Services/QuestService.cs
@@ -0,0 +1,37 @@
+﻿namespace QueReal.BLL.Services
+{
+    public class QuestService : IQuestService
+    {
+        private readonly IRepository<Quest> repository;
+        private readonly ICurrentUserService currentUserService;
+
+        public QuestService(IRepository<Quest> repository, ICurrentUserService currentUserService)
+        {
+            this.repository = repository;
+            this.currentUserService = currentUserService;
+        }
+
+        public Task<Guid> CreateAsync(Quest questModel)
+        {
+            questModel.CreatorId = currentUserService.UserId.Value;
+
+            return repository.CreateAsync(questModel);
+        }
+
+        public Task<IEnumerable<Quest>> GetAllAsync(int numberPage, int pageSize)
+        {
+            var skipCount = (numberPage - 1) * pageSize;
+
+            return repository.GetAllAsync(
+                predicate: quest => quest.CreatorId == currentUserService.UserId,
+                orderFunc: quests => quests.OrderByDescending(x => x.UpdateTime),
+                skipCount, 
+                pageSize);
+        }
+
+        public Task<Quest> GetAsync(Guid id)
+        {
+            return repository.GetAsync(quest => quest.Id == id);
+        }
+    }
+}
