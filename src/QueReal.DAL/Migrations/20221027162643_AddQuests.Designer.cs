57649f32fa6f07360a86dd4ed742182d39ac0c34
Андрей Стороженко <andreystorozhen4o@gmail.com>
2022-10-27 19:33:36 +0300

    Add migration for task

--- src/QueReal.DAL/Migrations/20221027162643_AddQuests.Designer.cs
+++ src/QueReal.DAL/Migrations/20221027162643_AddQuests.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QueReal.DAL.EF;
 
@@ -11,9 +12,10 @@ using QueReal.DAL.EF;
 namespace QueReal.DAL.Migrations
 {
     [DbContext(typeof(QueRealContext))]
-    partial class QueRealContextModelSnapshot : ModelSnapshot
+    [Migration("20221027162643_AddQuests")]
+    partial class AddQuests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
