f65062ec47185247b62f95e4368f7864c8b46154
Андрей Стороженко <andreystorozhen4o@gmail.com>
2023-04-17 18:35:37 +0300

    Add base page changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,34 +4,42 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     loadChildren: ()=>import("./pages/home/home-page/home-page.module").then(module=> module.HomePageModule),
   },
   {
-    path: '/quest/create',
+    path: 'quest/create',
+    pathMatch: 'full',
     loadChildren: ()=>import("./pages/quest/quest-create-page/quest-create-page.module").then(module=> module.QuestCreatePageModule),
   },
   {
-    path: '/quest/:questId/edit',
-    loadChildren: ()=>import("./pages/quest/quest-edit-page/quest-edit-page.module").then(module=> module.QuestEditPageModule),
-  },
-  {
-    path: '/quest/:questId',
-    loadChildren: ()=>import("./pages/quest/quest-view-page/quest-view-page.module").then(module=> module.QuestViewPageModule),
-  },
-  {
-    path: '/quest/list',
+    path: 'quest/list',
+    pathMatch: 'full',
     loadChildren: ()=>import("./pages/quest/quest-list-page/quest-list-page.module").then(module=> module.QuestListPageModule),
   },
   {
-    path: '/user/login',
+    path: 'quest/view/:questId',
+    pathMatch: 'full',
+    loadChildren: ()=>import("./pages/quest/quest-view-page/quest-view-page.module").then(module=> module.QuestViewPageModule),
+  },
+  {
+    path: 'quest/edit/:questId',
+    pathMatch: 'full',
+    loadChildren: ()=>import("./pages/quest/quest-edit-page/quest-edit-page.module").then(module=> module.QuestEditPageModule),
+  },
+  {
+    path: 'user/login',
+    pathMatch: 'full',
     loadChildren: ()=>import("./pages/user/user-login-page/user-login-page.module").then(module=> module.UserLoginPageModule),
   },
   {
-    path: '/user/register',
+    path: 'user/register',
+    pathMatch: 'full',
     loadChildren: ()=>import("./pages/user/user-register-page/user-register-page.module").then(module=> module.UserRegisterPageModule),
   },
   {
-    path: '/exception/no-access',
+    path: 'exception/no-access',
+    pathMatch: 'full',
     loadChildren: ()=>import("./pages/exception/no-access-page/no-access-page.module").then(module=> module.NoAccessPageModule),
   },
   {
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { AppCommonModule } from './common/app-common.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthorizationInterceptor } from './interceptors/authorization.interceptor';
+import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -12,9 +20,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    AppCommonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthorizationInterceptor,
+      multi: true,
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { dateFormat: "dd-MM-yyyy HH:mm:ss" },
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
