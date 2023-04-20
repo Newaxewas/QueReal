96d87c9177c987afb69df35dc3c74e248c74f961
Андрей Стороженко <andreystorozhen4o@gmail.com>
2023-04-20 19:57:01 +0300

    Add no access and not found pages

--- frontend/src/app/pages/exception/not-found-page/not-found-page.module.ts
+++ frontend/src/app/pages/exception/not-found-page/not-found-page.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: NotFoundPageComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NotFoundPageComponent
+  ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
